7fe0c3b79a7c838361e880ac2e63b9bf1227f83f
harsha270501 <61742450+harsha270501@users.noreply.github.com>
2020-07-11 21:31:07 +0530

    Added comments

--- pythonsourcecode/asearch.py
+++ pythonsourcecode/asearch.py
@@ -1,12 +1,16 @@
+# module - for A* search
+# imports graph module
 import graph as g
 
+# list walk- specifies if each cell is walkable or not
 walk = []
-dist = {}
+# list opt - specifies the options opted. opt[0]- option for Diagonal Movement, opt[1]- heuristics option if any
 opt = []
-
-
+# list endpt - endpt[0]- cell number , endpt[1] - x coordinate , endpt[2] - y coordinate
 endpt=[]
 
+# Function block_node changes the corresponding 'walk' value
+# @param blist - list of nodes that needs to be blocked
 def block_node(blist):
     for i in range(g.dim[0]*g.dim[1]):
         walk.append(True)
@@ -14,8 +18,13 @@ def block_node(blist):
     for i in blist:
         walk[i] = False
 
-
-
+# Function a_path calls the a_search function and traces back the path
+# @param option - list of options
+# @param s - start cell
+# @param e - end cell
+# @param blist - list of nodes that needs to be blocked
+# @return (trace,distance) - returns trace list and total distance on a successful search
+#                            else returns "Not Found"
 def a_path(options, s, e, blist):
     block_node(blist)
     #vertices : src, f, g, h
@@ -29,7 +38,9 @@ def a_path(options, s, e, blist):
     open_list={}
     open_list[s]=[-1,0,0,0]
     closed_list={}
-    a_search(open_list,closed_list)
+    a=a_search(open_list,closed_list)
+    if(a==-1):
+        return "Not Found"
     trace=[e]
     p=closed_list[e]
     while(p!=-1):
@@ -38,6 +49,10 @@ def a_path(options, s, e, blist):
     trace.reverse()
     return (trace,open_list[e][2])
 
+# Function a_search - performs the A* Search Algorithm
+# @param o - open list consists of the adjacent cells that are yet to be visited
+# @param c - closed list that contains the cells that have been visited
+# @return - returns 0 if successful else returns -1
 def a_search(o, c):
 
     while (len(o.keys())!=0):
@@ -52,7 +67,7 @@ def a_search(o, c):
                 minv=i
         c[minv] = o[minv][0]
         if(minv==endpt[0]):
-            return
+            return 0
         adj=g.get_neigh(minv,walk,opt[0])
         for i in adj:
             if i in c.keys():
@@ -70,6 +85,8 @@ def a_search(o, c):
                 h1=g.calc_dist(endpt[1],endpt[2],x,y,opt[1])
                 l=[minv,gd+h1,gd,h1]
                 o[i]=l
+        if minv == -1:
+            return -1
         del o[minv]
 
 
